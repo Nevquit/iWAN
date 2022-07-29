94d6fd8eda10b181f168b16ab838e165c4c42e49
GUO Wei <13911137413@163.com>
2022-07-29 14:24:23 +0800

    'update'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iWAN",
-    version="0.0.2",
+    version="0.0.3",
     author="vSir",
     author_email="weiguo341@gmail.com",
     description="simple tool",
